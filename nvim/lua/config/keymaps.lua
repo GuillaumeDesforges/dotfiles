51ca56b3939e30d8f6ba310d35c5396b0a9abb4d
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-11-19 09:18:43 +0100

    nvim: buffer navigation keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Add any additional keymaps here-- This file is automatically loaded by lazyvim.config.init
+
+
+local map = vim.keymap.set
+
+-- buffer navigation
+map("n", "<leader>bn", "<cmd>bn<cr>", { desc = "Next buffer" })
+map("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Delete buffer" })
