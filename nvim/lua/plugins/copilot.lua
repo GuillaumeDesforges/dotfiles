d5e9c648899034b50169bce1edb9a75842e00087
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2024-03-28 17:18:48 +0900

    nvim: add copilot chat

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -37,5 +37,20 @@ return {
       },
     },
   },
+  -- add 'copilot' component to lualine
   { "AndreM222/copilot-lualine" },
+  -- add Copilot Chat
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
