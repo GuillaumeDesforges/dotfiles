285300e4ba23a93cd30951c4bdc40657826222fc
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2024-10-04 21:09:17 +0200

    go: add formatter

--- nvim/lua/plugins/languages/go.lua
+++ nvim/lua/plugins/languages/go.lua
@@ -5,11 +5,32 @@ return {
     opts = {
       -- LSP Server Settings
       servers = {
-        gopls = {},
+        gopls = {
+          cmd = { "gopls", "serve" },
+          filetypes = { "go" },
+          root_dir = require("lspconfig.util").root_pattern("go.mod", ".git"),
+          settings = {
+            gopls = {
+              analyses = {
+                unusedparams = true,
+              },
+              staticcheck = true,
+            },
+          },
+        },
         templ = {},
       },
     },
   },
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			---@type table<string, conform.FormatterUnit[]>
+			formatters_by_ft = {
+				go = { { "gofmt" } },
+			},
+		},
+	},
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
