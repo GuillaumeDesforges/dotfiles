a4da5dde8e9e53c0c846cf01f5dde6f9cdbd87da
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2025-01-17 13:46:07 +0100

    nvim: use typescript-tools

--- nvim/lua/plugins/languages/typescript.lua
+++ nvim/lua/plugins/languages/typescript.lua
@@ -4,4 +4,15 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {},
   },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      -- LSP Server Settings
+      servers = {
+        -- use typescript-tools instead
+        tsserver = false,
+      },
+    },
+  },
 }
