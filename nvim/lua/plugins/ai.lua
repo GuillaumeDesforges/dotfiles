0f7b4c987711a1a7dfea39cdab6f4502db562017
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2025-02-24 18:20:23 +0100

    nvim: add nvim-aider

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -19,6 +19,7 @@ return {
           },
         },
         agents = {
+          -- TODO: turn into a proper agent with a good prompt
           {
             name = "Copilot",
             provider = "copilot",
@@ -26,10 +27,45 @@ return {
             command = true,
             model = { model = "gpt-4-turbo" },
             -- system_prompt = "Answer any query with just: Sure thing..",
+            disable = true,
           },
         },
       }
       require("gp").setup(conf)
     end,
+  },
+  {
+    "GeorgesAlkhouri/nvim-aider",
+    dependencies = {
+      "folke/snacks.nvim",
+      -- optional
+      "catppuccin/nvim",
+      "nvim-tree/nvim-tree.lua",
+      -- which-key integration
+      {
+        "folke/which-key.nvim",
+        optional = true,
+        opts = {
+          spec = {
+            { "<leader>a", group = "aider" },
+          },
+        },
+      },
+    },
+    cmd = {
+      "AiderTerminalToggle", "AiderHealth",
+    },
+    keys = {
+      { "<leader>a/", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
+      { "<leader>as", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
+      { "<leader>ac", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
+      { "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
+      { "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
+      { "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
+      { "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
+      { "<leader>a+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
+      { "<leader>a-", "<cmd>AiderTreeDropFile<cr>", desc = "Drop File from Tree from Aider", ft = "NvimTree" },
+    },
+    config = true,
   }
 }
