5b390bbd9a2ce95b4debff8adaa130513f1e3375
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2025-06-20 18:22:28 +0200

    nvim: fix typescript-tools

--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -1,11 +1,1 @@
-require("typescript-tools").setup({
-	settings = {
-		-- JSXCloseTag
-		-- WARNING: it is disabled by default (maybe you configuration or distro already uses nvim-ts-autotag,
-		-- that maybe have a conflict if enable this feature. )
-		jsx_close_tag = {
-			enable = true,
-			filetypes = { "javascriptreact", "typescriptreact" },
-		}
-	},
-})
+require("typescript-tools").setup({})
