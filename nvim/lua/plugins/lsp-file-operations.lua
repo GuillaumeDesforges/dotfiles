654d4b4037c0c94e0051fc6fb155e726e43114e6
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2025-06-23 14:27:39 +0200

    nvim: migrate nvim-lsp-file-operations

---
+++ nvim/lua/plugins/lsp-file-operations.lua
@@ -0,0 +1,31 @@
+require("lsp-file-operations").setup {
+	-- used to see debug logs in file `vim.fn.stdpath("cache") .. lsp-file-operations.log`
+	debug = false,
+	-- select which file operations to enable
+	operations = {
+		willRenameFiles = true,
+		didRenameFiles = true,
+		willCreateFiles = true,
+		didCreateFiles = true,
+		willDeleteFiles = true,
+		didDeleteFiles = true,
+	},
+	-- how long to wait (in milliseconds) for file rename information before cancelling
+	timeout_ms = 10000,
+}
+
+-- Set global defaults for all servers
+local lspconfig = require 'lspconfig'
+lspconfig.util.default_config = vim.tbl_extend(
+	'force',
+	lspconfig.util.default_config,
+	{
+		capabilities = vim.tbl_deep_extend(
+			"force",
+			vim.lsp.protocol.make_client_capabilities(),
+			-- returns configured operations if setup() was already called
+			-- or default operations if not
+			require 'lsp-file-operations'.default_capabilities()
+		)
+	}
+)
