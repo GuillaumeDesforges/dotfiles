da9ea9dac7b77060f5b88a37c42e130d37c89281
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2025-06-23 11:21:41 +0200

    nvim: keymaps in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,3 +27,52 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 require("bootstrap/rocks")
 
 -- plugin configs in `./lua/plugins/` are loaded by rocks-configs
+
+--
+-- key bindings
+--
+
+vim.keymap.set('n', '<leader>qq', ':qa<cr>', { desc = 'Quit' })
+
+vim.keymap.set('n', '<leader>bd', ':bd<cr>', { desc = 'Delete current buffer' })
+vim.keymap.set('n', '<leader>bn', ':bn<cr>', { desc = 'Next buffer' })
+vim.keymap.set('n', '<leader>bp', ':bp<cr>', { desc = 'Previous buffer' })
+vim.keymap.set('n', '<s-l>', ':bn<cr>', { desc = 'Next buffer' })
+vim.keymap.set('n', '<s-h>', ':bp<cr>', { desc = 'Previous buffer' })
+
+vim.keymap.set('n', '<leader>ws', '<c-w>s', { desc = "Split window horizontally" })
+vim.keymap.set('n', '<leader>wv', '<c-w>v', { desc = "Split window vertically" })
+vim.keymap.set('n', '<leader>wq', '<c-w>q', { desc = "Close window" })
+vim.keymap.set('n', '<leader>wo', '<c-w>o', { desc = "Close other windows" })
+vim.keymap.set('n', '<leader>ww', '<c-w>w', { desc = "Switch window" })
+vim.keymap.set('n', '<leader>wh', '<c-w>h', { desc = "Go to left window" })
+vim.keymap.set('n', '<leader>wh', '<c-w>j', { desc = "Go to down window" })
+vim.keymap.set('n', '<leader>wh', '<c-w>k', { desc = "Go to up window" })
+vim.keymap.set('n', '<leader>wh', '<c-w>l', { desc = "Go to right window" })
+vim.keymap.set('n', '<leader>wd', '<c-w>l', { desc = "Go to right window" })
+
+vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, { desc = "Go to definition" })
+vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, { desc = "Go to references" })
+
+vim.keymap.set('n', '<leader>gd', function() vim.lsp.buf.definition() end, { desc = "Go to definition" })
+vim.keymap.set('n', '<leader>gr', function() vim.lsp.buf.references() end, { desc = "Go to references" })
+vim.keymap.set('n', '<leader>ge',
+	function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR, wrap = true }) end,
+	{ desc = "Go to next error" }
+)
+vim.keymap.set('n', ']e',
+	function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR, wrap = true }) end,
+	{ desc = "Go to next error" }
+)
+vim.keymap.set('n', '[e',
+	function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR, wrap = true }) end,
+	{ desc = "Go to previous error" }
+)
+vim.keymap.set('n', ']w',
+	function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN, wrap = true }) end,
+	{ desc = "Go to next warning" }
+)
+vim.keymap.set('n', '[w',
+	function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN, wrap = true }) end,
+	{ desc = "Go to previous warning" }
+)
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -2,48 +2,9 @@ local wk = require("which-key")
 
 wk.setup {}
 
--- common key bindings to <leader>
 wk.add({
-	{ '<leader>b',  group = 'Buffers' },
-	{ '<leader>bd', ':bd<cr>',        desc = 'Delete current buffer' },
-	{ '<leader>bn', ':bn<cr>',        desc = 'Next buffer' },
-	{ '<leader>bp', ':bp<cr>',        desc = 'Previous buffer' },
-	{ '<s-l>',      ':bn<cr>',        desc = 'Next buffer' },
-	{ '<s-h>',      ':bp<cr>',        desc = 'Previous buffer' },
-})
-
-wk.add({
-	{ '<leader>q',  group = 'Session' },
-	{ '<leader>qq', ':qa<cr>',        desc = 'Quit' },
-})
-
-wk.add({
-	{ '<leader>w',  group = 'Windows' },
-	{ '<leader>ws', '<c-w>s',         desc = "Split window horizontally" },
-	{ '<leader>wv', '<c-w>v',         desc = "Split window vertically" },
-	{ '<leader>wq', '<c-w>q',         desc = "Close window" },
-	{ '<leader>wo', '<c-w>o',         desc = "Close other windows" },
-	{ '<leader>ww', '<c-w>w',         desc = "Switch window" },
-	{ '<leader>wh', '<c-w>h',         desc = "Go to left window" },
-	{ '<leader>wh', '<c-w>j',         desc = "Go to down window" },
-	{ '<leader>wh', '<c-w>k',         desc = "Go to up window" },
-	{ '<leader>wh', '<c-w>l',         desc = "Go to right window" },
-	{ '<leader>wd', '<c-w>l',         desc = "Go to right window" },
-})
-
-wk.add({
-	{ 'gd',         function() vim.lsp.buf.definition() end,                                                                desc = "Go to definition" },
-	{ 'gr',         function() vim.lsp.buf.references() end,                                                                desc = "Go to references" },
-
-	{ '<leader>w',  group = 'Go to' },
-	{ '<leader>gd', function() vim.lsp.buf.definition() end,                                                                desc = "Go to definition" },
-	{ '<leader>gr', function() vim.lsp.buf.references() end,                                                                desc = "Go to references" },
-	{ '<leader>ge', function() vim.lsp.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR, wrap = true }) end, desc = "Go to next error" },
-})
-
-wk.add({
-	{ ']e', function() vim.lsp.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR, wrap = true }) end,   desc = "Go to next error" },
-	{ '[e', function() vim.lsp.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR, wrap = true }) end,   desc = "Go to previous error" },
-	{ ']w', function() vim.lsp.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARNING, wrap = true }) end, desc = "Go to next warning" },
-	{ '[w', function() vim.lsp.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARNING, wrap = true }) end, desc = "Go to previous warning" },
+	{ '<leader>b', group = 'Buffers' },
+	{ '<leader>q', group = 'Session' },
+	{ '<leader>w', group = 'Windows' },
+	{ '<leader>w', group = 'Go to' },
 })
