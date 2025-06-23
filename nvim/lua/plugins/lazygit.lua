b4bfa9605ee7cc5f60e1ca97c88674ccbb0108e1
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2025-06-23 11:20:50 +0200

    lazygit: open in nvim instance

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -3,3 +3,23 @@ require("lazygit")
 require("which-key").add({
 	{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
 })
+
+-- from https://github.com/kdheepak/lazygit.nvim/issues/67
+function EditLineFromLazygit(file_path, line)
+	local path = vim.fn.expand("%:p")
+	if path == file_path then
+		vim.cmd(tostring(line))
+	else
+		vim.cmd("e " .. file_path)
+		vim.cmd(tostring(line))
+	end
+end
+
+function EditFromLazygit(file_path)
+	local path = vim.fn.expand("%:p")
+	if path == file_path then
+		return
+	else
+		vim.cmd("e " .. file_path)
+	end
+end
