8af474f4630654113a4ca7df8faa31c30ef57391
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2026-04-19 22:19:48 +0200

    nvim: make telescope fullscreen

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,11 +1,16 @@
-require("telescope").setup()
+require("telescope").setup({
+	defaults = {
+		layout_strategy = 'horizontal',
+		layout_config = { height = 0.99, width = 0.99 },
+	},
+})
 
 require("which-key").add({
-  { '<leader>/', ':Telescope live_grep<cr>', desc = 'Live grep' },
+	{ '<leader>/',  ':Telescope live_grep<cr>',  desc = 'Live grep' },
 
-  { '<leader>f', group = 'Find' },
-  { '<leader>ff', ':Telescope find_files<cr>', desc = 'Find files' },
-  { '<leader>fb', ':Telescope buffers<cr>', desc = 'Find files' },
-  { '<leader>fj', ':Telescope jumplist<cr>', desc = 'Find jump' },
-  { '<leader>fr', ':Telescope oldfiles<cr>', desc = 'Find recent file' },
+	{ '<leader>f',  group = 'Find' },
+	{ '<leader>ff', ':Telescope find_files<cr>', desc = 'Find files' },
+	{ '<leader>fb', ':Telescope buffers<cr>',    desc = 'Find files' },
+	{ '<leader>fj', ':Telescope jumplist<cr>',   desc = 'Find jump' },
+	{ '<leader>fr', ':Telescope oldfiles<cr>',   desc = 'Find recent file' },
 })
